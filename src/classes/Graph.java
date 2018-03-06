c5b1d59acccc4f07cb2291e2cf2bd490ef316194
camper9993 <danilov_1999@mail.ru>
2018-03-06 21:43:14 +0300

    test

--- src/classes/Graph.java
+++ src/classes/Graph.java
@@ -1,85 +1,74 @@
 package classes;
 
 import java.util.ArrayList;
+import java.util.List;
 
-    public class Graph {
+public class Graph {
         public ArrayList<String> vertexList = new ArrayList<>();
-        public final int Vertices = 100;
-        public int[][] matrix;
+        public Integer[][] matrix;
 
-        public Graph() {
-            matrix = new int[Vertices][Vertices];
+        public void graph(int Vertices) {
+            matrix = new Integer[Vertices][Vertices];
             for (int i = 0; i < Vertices; i++)
                 for (int j = 0; j < Vertices; j++)
-                    matrix[i][j] = 0;
+                    matrix[i][j] = null;
         }
 
-        public void AddVertex(String name) {
-            vertexList.add(name);
+        public void addVertex(String name) {
+            if (!vertexList.contains(name))
+                vertexList.add(name);
+
         }
 
-        public void AddRib(String vertex1, String vertex2, int ribPrice) {
+        public void changeName(String name1, String name2) {
+            int a = vertexList.indexOf(name1);
+            if (!vertexList.contains(name2))
+                 vertexList.set(a, name2);
+        }
+
+        public void addRib(String vertex1, String vertex2, Integer ribPrice) {
             matrix[vertexList.indexOf(vertex1)][vertexList.indexOf(vertex2)] = ribPrice;
         }
 
-        public void ChangeRibPrice(String vertex1, String vertex2, int ribPrice) {
-            if (matrix[vertexList.indexOf(vertex1)][vertexList.indexOf(vertex2)] != 0)
+        public void changeRibPrice(String vertex1, String vertex2, Integer ribPrice) {
+            if (matrix[vertexList.indexOf(vertex1)][vertexList.indexOf(vertex2)] != null)
                 matrix[vertexList.indexOf(vertex1)][vertexList.indexOf(vertex2)] = ribPrice;
         }
 
-        public void RemoveVertex(String name) {
+        public void removeVertex(String name) {
             int a = vertexList.indexOf(name);
             for (int i = 0;i < vertexList.size();i++) {
                 for (int j = 0;j < vertexList.size();j++) {
                     if (i < a && j >= a)
                         matrix[i][j] = matrix[i][j + 1];
+
                     if (i >= a && j >= a)
                         matrix[i][j] = matrix[i + 1][j + 1];
+
                     if (i >= a && j < a)
                         matrix[i][j] = matrix[i + 1][j];
+
                     if (i == vertexList.size() - 1 || j == vertexList.size() - 1)
-                        matrix[i][j] = 0;
+                        matrix[i][j] = null;
                 }
             }
             vertexList.remove(name);
         }
 
-        public ArrayList<String> NumberOfChildren(String name) {
-            ArrayList<String> list = new ArrayList();
-            for (int i = 0; i < Vertices; i++) {
-                if (matrix[vertexList.indexOf(name)][i] != 0)
+        public List<String> getChildren(String name) {
+            List<String> list = new ArrayList();
+            for (int i = 0; i < vertexList.size(); i++) {
+                if (matrix[vertexList.indexOf(name)][i] != null)
                     list.add(vertexList.get(i));
             }
             return list;
         }
-        public ArrayList<String> NumberOfParents(String name) {
-            ArrayList<String> list = new ArrayList();
-            for (int i = 0; i < Vertices; i++) {
-                if (matrix[i][vertexList.indexOf(name)] != 0)
+        public List<String> getParents(String name) {
+            List<String> list = new ArrayList();
+            for (int i = 0; i < vertexList.size(); i++) {
+                if (matrix[i][vertexList.indexOf(name)] != null)
                     list.add(vertexList.get(i));
             }
             return list;
         }
     }
-
-
-
-/** if (a == vertexList.size() - 1)
- vertexList.remove(name);
- else {
- for (int i = 0; i < vertexList.size() - 1; i++) {
- for (int j = 0;j < vertexList.size() - (a + 1);j++) {
- if (a + j != i) {
- matrix[a + j][i] = matrix[a + j][i + 1];
- matrix[i][a + j] = matrix[i + 1][a + j];
- }
- else
- matrix[a + j][i] = 0;
- }
- }
- vertexList.remove(name);
- }
- for (int i = 0;i <= vertexList.size();i++) {
- matrix[vertexList.size()][i] = 0;
- matrix[i][vertexList.size()] = 0;
- }*/
