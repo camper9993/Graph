7c4052a171330cf31e64a0c8f23ede354728d0e8
camper9993 <danilov_1999@mail.ru>
2018-03-11 01:39:35 +0300

    Best edition

--- src/classes/Graph.java
+++ src/classes/Graph.java
@@ -1,74 +1,57 @@
 package classes;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Graph {
-        public ArrayList<String> vertexList = new ArrayList<>();
-        public Integer[][] matrix;
+    private Map<String, Map<String, Integer>> matrixList = new HashMap<>();
 
-        public void graph(int Vertices) {
-            matrix = new Integer[Vertices][Vertices];
-            for (int i = 0; i < Vertices; i++)
-                for (int j = 0; j < Vertices; j++)
-                    matrix[i][j] = null;
-        }
+    void addVertex(String name) {
+        matrixList.put(name, new HashMap<>());
+    }
 
-        public void addVertex(String name) {
-            if (!vertexList.contains(name))
-                vertexList.add(name);
-
-        }
-
-        public void changeName(String name1, String name2) {
-            int a = vertexList.indexOf(name1);
-            if (!vertexList.contains(name2))
-                 vertexList.set(a, name2);
-        }
-
-        public void addRib(String vertex1, String vertex2, Integer ribPrice) {
-            matrix[vertexList.indexOf(vertex1)][vertexList.indexOf(vertex2)] = ribPrice;
-        }
-
-        public void changeRibPrice(String vertex1, String vertex2, Integer ribPrice) {
-            if (matrix[vertexList.indexOf(vertex1)][vertexList.indexOf(vertex2)] != null)
-                matrix[vertexList.indexOf(vertex1)][vertexList.indexOf(vertex2)] = ribPrice;
-        }
-
-        public void removeVertex(String name) {
-            int a = vertexList.indexOf(name);
-            for (int i = 0;i < vertexList.size();i++) {
-                for (int j = 0;j < vertexList.size();j++) {
-                    if (i < a && j >= a)
-                        matrix[i][j] = matrix[i][j + 1];
-
-                    if (i >= a && j >= a)
-                        matrix[i][j] = matrix[i + 1][j + 1];
-
-                    if (i >= a && j < a)
-                        matrix[i][j] = matrix[i + 1][j];
-
-                    if (i == vertexList.size() - 1 || j == vertexList.size() - 1)
-                        matrix[i][j] = null;
-                }
+    void changeName(String name1, String name2) {
+        matrixList.put(name2, matrixList.get(name1));
+        matrixList.remove(name1);
+        for (String key : matrixList.keySet()) {
+            if (matrixList.get(key) != null && matrixList.get(key).containsKey(name1)) {
+                matrixList.get(key).put(name2, matrixList.get(key).get(name1));
+                matrixList.get(key).remove(name1);
             }
-            vertexList.remove(name);
-        }
-
-        public List<String> getChildren(String name) {
-            List<String> list = new ArrayList();
-            for (int i = 0; i < vertexList.size(); i++) {
-                if (matrix[vertexList.indexOf(name)][i] != null)
-                    list.add(vertexList.get(i));
-            }
-            return list;
-        }
-        public List<String> getParents(String name) {
-            List<String> list = new ArrayList();
-            for (int i = 0; i < vertexList.size(); i++) {
-                if (matrix[i][vertexList.indexOf(name)] != null)
-                    list.add(vertexList.get(i));
-            }
-            return list;
         }
     }
+
+    void addRib(String name1, String name2, Integer ribPrice) {
+        matrixList.get(name1).put(name2, ribPrice);
+    }
+
+    void changeRibPrice(String name1, String name2, Integer ribPrice) {
+        matrixList.get(name1).put(name2, ribPrice);
+    }
+
+    void deleteRib(String name1, String name2) {
+        matrixList.get(name1).remove(name2);
+    }
+
+    void removeVertex(String name) {
+        matrixList.remove(name);
+        for (String key : matrixList.keySet()) {
+            if (matrixList.get(key) != null && matrixList.get(key).containsKey(name))
+                matrixList.get(key).remove(name);
+        }
+    }
+
+    List<String> getParents(String name) {
+        ArrayList<String> list = new ArrayList<>();
+        for (String key : matrixList.keySet()) {
+            if (matrixList.get(key) != null && matrixList.get(key).containsKey(name))
+                list.add(key);
+        }
+        return list;
+    }
+
+    List<String> getChildren(String name) {
+        ArrayList<String> list = new ArrayList<>();
+        list.addAll(matrixList.get(name).keySet());
+        return list;
+    }
+}
