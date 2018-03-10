47554dc65d3f23f9a3d2c2d8db63038203d48c12
camper9993 <danilov_1999@mail.ru>
2018-03-11 01:39:49 +0300

    Best edition

--- src/classes/GraphTest.java
+++ src/classes/GraphTest.java
@@ -2,6 +2,9 @@ package classes;
 
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GraphTest {
@@ -9,9 +12,8 @@ class GraphTest {
     @Test
     void NumberOfChildren() {
         ArrayList<String> list = new ArrayList<>();
-        list.add("K");
         list.add("F");
-        graph.graph(8);
+        list.add("K");
         graph.addVertex("A");
         graph.addVertex("B");
         graph.addVertex("C");
@@ -23,7 +25,7 @@ class GraphTest {
         graph.addRib("E","D",2);
         graph.addRib("C","A",1);
         graph.addRib("E","F",2);
-        graph.changeRibPrice("E","C",null);
+        graph.deleteRib("E","C");
         graph.removeVertex("B");
         graph.changeName("D","K");
         assertEquals(list,graph.getChildren("E"));
@@ -32,22 +34,25 @@ class GraphTest {
     void NumberOfParents() {
         ArrayList<String> list = new ArrayList<>();
         list.add("B");
-        graph.graph(8);
+        list.add("U");
+        list.add("G");
         graph.addVertex("A");
         graph.addVertex("B");
         graph.addVertex("C");
         graph.addVertex("D");
         graph.addVertex("E");
         graph.addVertex("F");
+        graph.addVertex("G");
         graph.addRib("A","E",6);
         graph.addRib("C","E", 10);
         graph.addRib("A","D",2);
         graph.addRib("F","E",8);
         graph.addRib("B","E",2);
-        graph.changeRibPrice("A","E",null);
+        graph.addRib("G","E",2);
+        graph.changeRibPrice("G","E",8);
+        graph.deleteRib("A","E");
         graph.removeVertex("C");
         graph.changeName("F","U");
-        graph.removeVertex("U");
         assertEquals(list,graph.getParents("E"));
     }
 }
